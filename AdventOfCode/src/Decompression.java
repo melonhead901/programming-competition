a437dfc324dbe5e23be57780d63b13acdbc98b0d
Kellen Donohue <kdonohue@palantir.com>
2016-12-08 23:02:54 -0800

    First round of refactoring

--- AdventOfCode/src/Decompression.java
+++ AdventOfCode/src/Decompression.java
@@ -5,36 +5,16 @@ import java.util.Scanner;
  * Created by kdonohue on 12/8/16.
  */
 public class Decompression {
-    private long length;
+    private final String stringToDecompress;
 
-    public Decompression() {
-        this.length = 0L;
+    public Decompression(String stringToDecompress) {
+        this.stringToDecompress = stringToDecompress;
     }
 
     public static void main(String[] args) {
-        Decompression decompression = new Decompression();
         Scanner in = new Scanner(System.in);
-        decompression.decompress(in.nextLine());
-        System.out.println(decompression.getLength());
-    }
-
-    public void decompress(String s) {
-        int i = 0;
-        while (i < s.length()) {
-            if (s.charAt(i) == '(') {
-                Optional<Marker> markerOptional = canGrabMarker(s.substring(i));
-                if (markerOptional.isPresent()) {
-                    System.err.println("Found pattern starting at" + s.substring(i));
-                    length += markerOptional.get().getExpandedLength();
-                    i += markerOptional.get().getReplacedLength();
-                    continue;
-                }
-            }
-            System.err.println("Processing " + s.charAt(i));
-            // Continue as normal.
-            i++;
-            length++;
-        }
+        Decompression decompression = new Decompression(in.nextLine());
+        System.out.println(decompression.getDecompressedLength());
     }
 
     private Optional<Marker> canGrabMarker(String substring) {
@@ -53,7 +33,7 @@ public class Decompression {
         try {
             int length = Integer.valueOf(splitForLength[0]);
             int repeats = Integer.valueOf(splitForRepeats[0]);
-            int lengthOfMarker = String.format("(%sx%s)", length, repeats).length();
+            int lengthOfMarker = Marker.regenerateMarkerLength(length, repeats);
             // The minus 1 is because we stripped off the original open paren.
             String containedSection = substring.substring(lengthOfMarker - 1, (lengthOfMarker - 1) + length);
             return Optional.of(new Marker(length, repeats, containedSection));
@@ -62,7 +42,24 @@ public class Decompression {
         }
     }
 
-    public long getLength() {
+    public long getDecompressedLength() {
+        int i = 0;
+        int length = 0;
+        while (i < stringToDecompress.length()) {
+            if (stringToDecompress.charAt(i) == '(') {
+                Optional<Marker> markerOptional = canGrabMarker(stringToDecompress.substring(i));
+                if (markerOptional.isPresent()) {
+                    System.err.println("Found pattern starting at" + stringToDecompress.substring(i));
+                    length += markerOptional.get().getExpandedLength();
+                    i += markerOptional.get().getReplacedLength();
+                    continue;
+                }
+            }
+            System.err.println("Processing " + stringToDecompress.charAt(i));
+            // Continue as normal.
+            i++;
+            length++;
+        }
         return length;
     }
 }
@@ -79,12 +76,15 @@ class Marker {
     }
 
     public long getExpandedLength() {
-        Decompression decompression = new Decompression();
-        decompression.decompress(containedSection);
-        return decompression.getLength() * repeats;
+        Decompression decompression = new Decompression(containedSection);
+        return decompression.getDecompressedLength() * repeats;
     }
 
     public long getReplacedLength() {
-        return String.format("(%sx%s)", length, repeats).length() + length;
+        return regenerateMarkerLength(length, repeats) + length;
+    }
+
+    public static int regenerateMarkerLength(long length, int repeats) {
+        return String.format("(%sx%s)", length, repeats).length();
     }
 }
