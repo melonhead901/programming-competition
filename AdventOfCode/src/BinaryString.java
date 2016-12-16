13f6e8cecbae0dbe8e5639a9bd2f38a9144bd7ba
Kellen Donohue <kdonohue@palantir.com>
2016-12-15 22:05:02 -0800

    More refactoring

--- AdventOfCode/src/BinaryString.java
+++ AdventOfCode/src/BinaryString.java
@@ -2,28 +2,85 @@ import java.util.Scanner;
 
 public class BinaryString {
 
-    private String string;
-    private final int requiredLength;
+    private DigitString string;
+    private final RequiredStringLength requiredLength;
 
     private BinaryString(String string, int requiredLength) {
-        this.string = string;
-        this.requiredLength = requiredLength;
+        this.string = new DigitString(string);
+        this.requiredLength = new RequiredStringLength(requiredLength);
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        BinaryString binaryString = new BinaryString(in.nextLine().trim(), 272);
+        BinaryString binaryString = new BinaryString(in.nextLine().trim(), 35651584);
         binaryString.randomize();
         binaryString.printChecksumLength();
     }
 
     private void randomize() {
         while (!longEnough()) {
-            String copy = string;
-            copy = new StringBuilder(copy).reverse().toString();
-            copy = flipBits(copy);
-            string = string + "0" + copy;
+            string = string.extend();
         }
+    }
+
+    private void printChecksumLength() {
+        if (!longEnough()) {
+            throw new IllegalStateException();
+        }
+
+        Checksum checkSum = string.createChecksum(requiredLength);
+
+        while (!checkSum.isReady()) {
+            checkSum = checkSum.performSingleChecksumRound();
+        }
+
+        System.out.println(checkSum);
+    }
+
+    private boolean longEnough() {
+        return string.satisfiesLength(requiredLength);
+    }
+
+}
+
+class Checksum extends DigitString {
+    public Checksum(String bareString) {
+        super(bareString);
+    }
+
+    public boolean isReady() {
+        return (string.length() % 2) != 0;
+    }
+
+    public Checksum performSingleChecksumRound() {
+        StringBuilder newCheckSum = new StringBuilder();
+        for (int i = 0; i < string.length(); i += 2) {
+            char c1 = string.charAt(i);
+            char c2 = string.charAt(i + 1);
+            if (c1 == c2) {
+                newCheckSum.append("1");
+            } else {
+                newCheckSum.append("0");
+            }
+        }
+        return new Checksum(newCheckSum.toString());
+    }
+
+}
+
+class DigitString {
+    protected String string;
+
+    public DigitString(String bareString) {
+        this.string = bareString;
+    }
+
+    public DigitString extend() {
+        String copy = string;
+        copy = new StringBuilder(copy).reverse().toString();
+        copy = flipBits(copy);
+        string = string + "0" + copy;
+        return new DigitString(copy);
     }
 
     private String flipBits(String string) {
@@ -47,36 +104,23 @@ public class BinaryString {
         }
     }
 
-    private void printChecksumLength() {
-        if (!longEnough()) {
-            throw new IllegalStateException();
-        }
-
-        String checkSum = string.substring(0, requiredLength);
-
-        while ((checkSum.length() % 2) == 0) {
-            checkSum = performSingleChecksumRound(checkSum);
-        }
-
-        System.out.println(checkSum);
+    boolean satisfiesLength(RequiredStringLength requiredStringLength) {
+        return string.length() > requiredStringLength.length;
     }
 
-    private String performSingleChecksumRound(String checkSum) {
-        StringBuilder newCheckSum = new StringBuilder();
-        for (int i = 0; i < checkSum.length(); i += 2) {
-            char c1 = checkSum.charAt(i);
-            char c2 = checkSum.charAt(i + 1);
-            if (c1 == c2) {
-                newCheckSum.append("1");
-            } else {
-                newCheckSum.append("0");
-            }
-        }
-        return  newCheckSum.toString();
+    public String trimToLength(RequiredStringLength requiredLength) {
+        return string.substring(0, requiredLength.length);
     }
 
-    private boolean longEnough() {
-        return string.length() >= requiredLength;
+    public Checksum createChecksum(RequiredStringLength requiredLength) {
+        return new Checksum(trimToLength(requiredLength));
     }
+}
 
+class RequiredStringLength {
+    final int length;
+
+    RequiredStringLength(int length) {
+        this.length = length;
+    }
 }
