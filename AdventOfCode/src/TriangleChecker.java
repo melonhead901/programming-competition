e3df18f6b5253e1867a77ee6b9a26a43f73aaf7e
Kellen Donohue <kdonohue@palantir.com>
2016-12-17 10:46:17 -0800

    Day 3 part 2

--- AdventOfCode/src/TriangleChecker.java
+++ AdventOfCode/src/TriangleChecker.java
@@ -1,43 +1,66 @@
 import org.jetbrains.annotations.Contract;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
  * Created by kdonohue on 12/16/16.
  */
 public class TriangleChecker {
-    private long numValidTriangles;
+    private final List<int[]> lines;
 
-    public TriangleChecker() {
-        numValidTriangles = 0;
+    public TriangleChecker(List<int[]> lines) {
+        this.lines = lines;
     }
 
+
     public static void main(String[] args) {
-        TriangleChecker triangleChecker = new TriangleChecker();
+        TriangleChecker triangleChecker = new TriangleChecker(new ArrayList<>());
         Scanner in = new Scanner(System.in);
         while (in.hasNext()) {
-            triangleChecker.addPotentialTriangle(in);
+            triangleChecker.addNextLine(in);
         }
         triangleChecker.printValidTriangleCount();
 
     }
 
-    private void printValidTriangleCount() {
-        System.out.print(numValidTriangles);
+    private void addNextLine(Scanner in) {
+        lines.add(parseInputString(in));
     }
 
-    private void addPotentialTriangle(Scanner in) {
-        int[] nums = parseInputString(in);
-        if (isValidTriangle(nums)) {
-            numValidTriangles++;
+    private void printValidTriangleCount() {
+        int numValidTriangles = 0;
+        int numOverThree = lines.size() / 3;
+        for (int i = 0; i < numOverThree; i++) {
+            int firstRow = (i) * 3;
+            if (isValidTriangle(new int[]{
+                lines.get(firstRow + 0)[0],
+                lines.get(firstRow + 1)[0],
+                lines.get(firstRow + 2)[0] })) {
+                numValidTriangles++;
+            }
+            if (isValidTriangle(new int[]{
+                lines.get(firstRow + 0)[1],
+                lines.get(firstRow + 1)[1],
+                lines.get(firstRow + 2)[1] })) {
+                numValidTriangles++;
+            }
+            if (isValidTriangle(new int[]{
+                lines.get(firstRow + 0)[2],
+                lines.get(firstRow + 1)[2],
+                lines.get(firstRow + 2)[2] })) {
+                numValidTriangles++;
+            }
         }
+        System.out.print(numValidTriangles);
     }
 
     @Contract(pure = true)
     private boolean isValidTriangle(int[] nums) {
-        return ((nums[0] + nums[1]) > nums[2]) &&
-            ((nums[1] + nums[2]) > nums[0]) &&
-            ((nums[0] + nums[2]) > nums[1]);
+        System.out.println("Considering " + Arrays.toString(nums));
+        return ((nums[0] + nums[1]) > nums[2]) && ((nums[1] + nums[2]) > nums[0]) && ((nums[0] + nums[2]) > nums[1]);
     }
 
     private int[] parseInputString(Scanner in) {
