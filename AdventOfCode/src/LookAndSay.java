5b4c16fb3b6e8ab716415ed84ec07008981f9ee2
Kellen Donohue <kdonohue@palantir.com>
2016-12-19 01:02:04 -0800

    Day 10 part 1

---
+++ AdventOfCode/src/LookAndSay.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+/**
+ * Created by kdonohue on 12/19/16.
+ */
+public class LookAndSay {
+    private String currentString;
+
+    public static final int NUM_ITERATIONS = 40;
+
+    public LookAndSay(String s) {
+        this.currentString = s;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        LookAndSay lookAndSay = new LookAndSay(in.nextLine());
+        for (int i = 0; i < NUM_ITERATIONS; i++) {
+            lookAndSay.iterate();
+        }
+        lookAndSay.printResultLength();
+    }
+
+    private void printResultLength() {
+        System.out.print(currentString.length());
+    }
+
+    private void iterate() {
+        int currentPosition = 0;
+        char lastChar = currentString.charAt(currentPosition);
+        int runLength = 0;
+        StringBuilder newString = new StringBuilder();
+        while (currentPosition < currentString.length()) {
+            if (currentString.charAt(currentPosition) != lastChar) {
+                newString.append(runLength);
+                newString.append(lastChar);
+                runLength = 0;
+            }
+            lastChar = currentString.charAt(currentPosition);
+            runLength++;
+            currentPosition++;
+        }
+        newString.append(runLength);
+        newString.append(lastChar);
+        currentString = newString.toString();
+        System.out.println(currentString);
+    }
+
+}
