2b0890c7b3db555432ebd60396510a1b72ce1ea5
Kellen Donohue <kdonohue@palantir.com>
2016-12-19 21:34:06 -0800

    day 20 part 2

--- AdventOfCode/src/adventofcode_2015/Day20.java
+++ AdventOfCode/src/adventofcode_2015/Day20.java
@@ -14,34 +14,67 @@ public class Day20 {
             String[] words = line.split("-");
             Restriction restriction = new Restriction(words[0], words[1]);
             day20.addRestriction(restriction);
-            day20.processRestrictions();
 
         }
-        day20.printMinVal();
+        day20.processRestrictions();
     }
 
     private void processRestrictions() {
-        Collections.sort(restrictions, Comparator.comparingLong(o -> o.min));
-        System.out.println(restrictions);
-        for (Restriction restriction : restrictions) {
-            if ((restriction.min <= minVal) || ((restriction.min >= minVal) && (restriction.max <= minVal))) {
-                minVal = Math.max(minVal, restriction.max + 1);
+        Collections.sort(events);
+        long vals = 0;
+        int activeRestrictions = 0;
+        for (int i = 0; i < (events.size() - 1); i++) {
+            Event e = events.get(i);
+            if (e instanceof Start) {
+                activeRestrictions++;
+            } else {
+                activeRestrictions--;
+                if (activeRestrictions == 0) {
+                    vals += (events.get(i + 1).loc) - (e.loc + 1);
+                }
             }
         }
+
+        vals += Math.pow(2, 32) - 1 - events.get(events.size() - 1).loc;
+
+        System.out.println(vals);
+
     }
 
-    private void printMinVal() {
-        System.out.print(minVal);
-    }
 
-
-    long minVal = 0;
-    List<Restriction> restrictions = new ArrayList<>();
+    List<Event> events = new ArrayList<>();
 
     private void addRestriction(Restriction restriction) {
-        restrictions.add(restriction);
+        events.add(new Start(restriction.min));
+        events.add(new End(restriction.max));
     }
 }
+
+class Start extends Event {
+    public Start(long loc) {
+        super(loc);
+    }
+}
+
+class End extends Event {
+    public End(long loc) {
+        super(loc);
+    }
+}
+
+abstract class Event implements Comparable<Event> {
+    long loc;
+
+    public Event(long loc) {
+        this.loc = loc;
+    }
+
+    @Override
+    public int compareTo(Event o) {
+        return Long.compare(this.loc, o.loc);
+    }
+}
+
 
 class Restriction {
     public Restriction(String min, String max) {
