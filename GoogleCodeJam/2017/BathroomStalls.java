7d26bf7580678d06e629a27595d4c65a465c9afb
Kellen Donohue <kdonohue@palantir.com>
2017-04-08 02:50:19 -0700

    Bad bathoom stalls, but sufficient

--- GoogleCodeJam/2017/BathroomStalls.java
+++ GoogleCodeJam/2017/BathroomStalls.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
  * Created by kdonohue on 4/8/17.
  */
 public class BathroomStalls {
+
+    public static final int SMALL = -1000000;
+    public static final int LARGE = 1000000;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int numCases = in.nextInt();
@@ -11,6 +15,9 @@ public class BathroomStalls {
             System.out.println("Case #" + i + ": " + processCase(in));
         }
     }
+
+    static int maxSeenLSRS;
+    static int minSeenLSRS;
 
     private static String processCase(Scanner in) {
         int n = in.nextInt();
@@ -21,16 +28,36 @@ public class BathroomStalls {
         for (int i = 0; i < k; i++) {
             recalculateDistL(distL, occupied);
             recalculateDistR(distR, occupied);
-            int seatToOccupy = chooseSeatToOccupy(distL, distR);
+            int seatToOccupy = chooseSeatToOccupy(occupied, distL, distR);
             occupied[seatToOccupy] = true;
         }
 
-        return null;
+        return String.format("%s %s", maxSeenLSRS, minSeenLSRS);
 
     }
 
-    private static int chooseSeatToOccupy(int[] distL, int[] distR) {
-        return 0;
+    private static int chooseSeatToOccupy(boolean[] occupied, int[] distL, int[] distR) {
+        maxSeenLSRS = SMALL;
+        minSeenLSRS = SMALL;
+        int seat = 0;
+        for (int i = 0; i < distL.length; i++) {
+            if (occupied[i]) {
+                continue;
+            }
+            int myMinLSRS = Math.min(distL[i], distR[i]);
+            int myMaxLSRS = Math.max(distL[i], distR[i]);
+            if (myMinLSRS > minSeenLSRS) {
+                minSeenLSRS = myMinLSRS;
+                maxSeenLSRS = myMaxLSRS;
+                seat = i;
+            } else if (myMinLSRS == minSeenLSRS) {
+                if (myMaxLSRS > maxSeenLSRS) {
+                    maxSeenLSRS = myMaxLSRS;
+                    seat = i;
+                }
+            }
+        }
+        return seat;
     }
 
     private static void recalculateDistR(int[] distR, boolean[] occupied) {
@@ -45,8 +72,8 @@ public class BathroomStalls {
     private static void recalculateDistL(int[] distL, boolean[] occupied) {
         for (int i = distL.length - 1; i >= 0; i--) {
             distL[i] = 0;
-            for (int j = i - 1; (j >= 0) && !occupied[j]; j--))))) {
-                distL[i]--;
+            for (int j = i - 1; (j >= 0) && !occupied[j]; j--) {
+                distL[i]++;
             }
         }
     }
