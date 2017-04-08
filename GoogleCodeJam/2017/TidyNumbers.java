5625d03921d74679d10a7cd4d5f9fd32e1b83c6a
Kellen Donohue <kdonohue@palantir.com>
2017-04-08 02:28:33 -0700

    Messed up Tidy numbers

--- GoogleCodeJam/2017/TidyNumbers.java
+++ GoogleCodeJam/2017/TidyNumbers.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -13,21 +14,33 @@ public class TidyNumbers {
     }
 
     private static String processCase(Scanner in) {
-        int n = in.nextInt();
-        int lastTidy = 1;
-        for (int i = 0; i <= n; i++) {
-            if (isTidy(i)) {
-                lastTidy = i;
+        BigInteger n = in.nextBigInteger();
+        while (!isTidy(n.toString())) {
+
+            char[] chars = n.toString().toCharArray();
+            for (int i = chars.length - 1; i > 0; i--) {
+                if (chars[i] < chars[i - 1]) {
+                    chars[i] = '9';
+                    while (chars[i-1] == '0') {
+                        chars[i-1] = '9';
+                        chars[i-2]--;
+                        i--;
+                    }
+                    chars[i-1]--;
+                }
+                //System.out.println(chars);
             }
+            n = new BigInteger(new String(chars));
         }
-        return lastTidy + "";
+
+        return n.toString();
     }
 
-    private static boolean isTidy(int n) {
-        char[] chars = (n + "").toCharArray();
+    private static boolean isTidy(String str) {
+        char[] chars = str.toCharArray();
         for (int i = 0; i + 1 < chars.length; i++) {
-            if (chars[i] > chars[i+1]) {
-                return  false;
+            if (chars[i] > chars[i + 1]) {
+                return false;
             }
         }
         return true;
