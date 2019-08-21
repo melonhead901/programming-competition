52fa9451fe14509d431b34d4dac93905a043c7db
Kellen Donohue <kdonohue@palantir.com>
2019-08-21 14:40:09 -0500

    fix class name

--- HackerRank/src/DynamicArray.java
+++ HackerRank/src/DynamicArray.java
@@ -87,7 +87,7 @@ class Result {
 
 }
 
-public class Solution {
+public class DynamicArray {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
