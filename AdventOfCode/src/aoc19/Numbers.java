0710b253cc9a2be5b4a10d908f031c39544d8fe3
Kellen Donohue <kdonohue@palantir.com>
2019-12-04 00:20:11 -0500

    day 4 2

--- AdventOfCode/src/aoc19/Numbers.java
+++ AdventOfCode/src/aoc19/Numbers.java
@@ -14,15 +14,40 @@ public class Numbers {
         System.out.println(count);
     }
 
+    private static String runLengthEncoding(String s) {
+        StringBuilder result = new StringBuilder();
+        char prev = s.charAt(0);
+        int length = 1;
+        for (int i = 1; i < s.length(); i++) {
+           if (s.charAt(i) == prev) {
+               length++;
+           } else {
+               result.append(length);
+               result.append(prev);
+               prev = s.charAt(i);
+               length = 1;
+           }
+        }
+        result.append(length);
+        result.append(prev);
+        return result.toString();
+    }
+
     private static boolean passesTest(int number) {
         boolean twoAdjSame = false;
-        char[] chars = (number + "").toCharArray();
+        String strRep = number + "";
+        char[] chars = strRep.toCharArray();
         for (int i = 0; i < (chars.length - 1); i++) {
-            if (chars[i] == chars[i + 1]) {
-                twoAdjSame = true;
-            }
             if (chars[i] > chars[i + 1]) {
                 return false;
+            }
+        }
+        String runLengthEncoding = runLengthEncoding(strRep);
+        System.out.println(String.format("RLE of %s is %s", strRep, runLengthEncoding));
+        for (int ii = 0; ii < runLengthEncoding.length(); ii += 2) {
+            if (runLengthEncoding.charAt(ii) == '2') {
+                twoAdjSame = true;
+                break;
             }
         }
         if (twoAdjSame) {
