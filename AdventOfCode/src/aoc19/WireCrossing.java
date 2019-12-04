1818cc219a0b2c25394ba91dc2c2f66991d8f386
Kellen Donohue <kdonohue@palantir.com>
2019-12-04 00:04:44 -0500

    day 3 2

--- AdventOfCode/src/aoc19/WireCrossing.java
+++ AdventOfCode/src/aoc19/WireCrossing.java
@@ -7,41 +7,57 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
 public class WireCrossing {
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("/Users/kdonohue/Downloads/day3.txt"));
-      //  lines = ImmutableList.of( "R8,U5,L5,D3",
-                //"U7,R6,D4,L4");
+          //lines = ImmutableList.of( "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+        //"U62,R66,U55,R34,D71,R55,D58,R83");
         List<Path> line1Dirs = parseDirs(lines.get(0));
         Set<Position> positions = new HashSet<>();
+        Map<Position, Integer> wire1Steps = new HashMap<>();
         Position position = Position.CENTRAL_PORT;
-        for (Path p : line1Dirs) {
-            System.out.println("Starting pos " + position );
-            positions.addAll(position.positionsAlongPath(p));
-            System.out.println(position.positionsAlongPath(p));
-            position = position.moveAlongPath(p);
+        int steps1 = 0;
+        for (Path path : line1Dirs) {
+            //System.out.println("Starting pos " + position);
+            List<Position> positionsAlongPath = position.positionsAlongPath(path);
+            positions.addAll(positionsAlongPath);
+            for (Position p : positionsAlongPath) {
+                if (!wire1Steps.containsKey(p)) {
+                    wire1Steps.put(p, steps1);
+                }
+                steps1++;
+            }
+            System.out.println(positionsAlongPath);
+            position = position.moveAlongPath(path);
         }
-        List<Position> matchingPositions = new ArrayList<>();
         List<Path> line2Dirs = parseDirs(lines.get(1));
         positions.remove(Position.CENTRAL_PORT);
         position = Position.CENTRAL_PORT;
+        Map<Position, Integer> combinedSteps = new HashMap<>();
+        int steps2 = 0;
+        List<Position> matchingPositions = new ArrayList<>();
         for (Path p : line2Dirs) {
             for (Position pathPos : position.positionsAlongPath(p)) {
                 if (positions.contains(pathPos)) {
                     matchingPositions.add(pathPos);
+                    if (!combinedSteps.containsKey(pathPos)) {
+                        combinedSteps.put(pathPos, wire1Steps.get(pathPos) + steps2);
+                    }
                 }
+                steps2++;
             }
             position = position.moveAlongPath(p);
         }
 
-        Position bestPos = matchingPositions.stream().min(Comparator.comparingInt(Position::manhattanDistance)).get();
-        System.out.println(bestPos);
-        System.out.println(bestPos.manhattanDistance());
+        Map.Entry<Position, Integer> bestPos = combinedSteps.entrySet().stream().min(Comparator.comparingInt(Map.Entry::getValue)).get();
+        System.out.println(bestPos.getValue());
 
     }
 
@@ -55,6 +71,39 @@ public class WireCrossing {
         }
         return result;
     }
+}
+
+class Match {
+    final Position position;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Match match = (Match) o;
+        return wire1Steps == match.wire1Steps &&
+                wire2Steps == match.wire2Steps &&
+                position.equals(match.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(position, wire1Steps, wire2Steps);
+    }
+
+    public Match(Position position, int wire1Steps, int wire2Steps) {
+        this.position = position;
+        this.wire1Steps = wire1Steps;
+        this.wire2Steps = wire2Steps;
+    }
+
+    final int wire1Steps;
+    int wire2Steps;
+
+    final int totalDistance() {
+        return wire1Steps + wire2Steps;
+    }
+
 }
 
 class Path {
