912dde95835aba4f394351bc07db276ff3644908
Kellen Donohue <kdonohue@palantir.com>
2020-06-09 10:24:48 -0700

    lint

--- HackerRank/src/HashingSha.java
+++ HackerRank/src/HashingSha.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class HashingSha {
     private HashingSha() {}
-    
+
     public static void main(String[] args) throws NoSuchAlgorithmException {
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
