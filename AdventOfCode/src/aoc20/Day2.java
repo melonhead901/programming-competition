92159bee824e76cafe2034274ee509dbc2ae125c
Kellen Donohue <kdonohue@palantir.com>
2020-12-02 14:27:37 -0800

    refactor

--- AdventOfCode/src/aoc20/Day2.java
+++ AdventOfCode/src/aoc20/Day2.java
@@ -21,33 +21,42 @@ public class Day2 {
     }
 
     private static boolean processCase(String line) {
-        Scanner wordScanner = new Scanner(line);
-        String count = wordScanner.next();
-        String[] countSplits = count.split("-");
-        int min = Integer.parseInt(countSplits[0]);
-        int max = Integer.parseInt(countSplits[1]);
-        String character = wordScanner.next();
-        char target = character.charAt(0);
-        String password = wordScanner.next();
+        State state = new State(line);
         int charCount = 0;
-        for (char c : password.toCharArray()) {
-            if (c == target) {
+        for (char c : state.password.toCharArray()) {
+            if (c == state.target) {
                 charCount++;
             }
         }
-        System.out.println(password);
-        return (charCount >= min) && (charCount <= max);
+        System.out.println(state.password);
+        return (charCount >= state.firstNum) && (charCount <= state.secondNum);
     }
 
     private static boolean processCase2(String line) {
+        State state = new State(line);
+        return (state.password.charAt(state.firstNum - 1) == state.target)
+                ^ (state.password.charAt(state.secondNum - 1) == state.target);
+    }
+}
+
+class State {
+    final int firstNum;
+    final int secondNum;
+    final char target;
+    final String password;
+
+    public State(String line) {
         Scanner wordScanner = new Scanner(line);
-        String count = wordScanner.next();
-        String[] countSplits = count.split("-");
-        int firstIndex = Integer.parseInt(countSplits[0]) - 1;
-        int secondIndex = Integer.parseInt(countSplits[1]) - 1;
+        String[] countSplits = getCountSplits(wordScanner);
+        this.firstNum = Integer.parseInt(countSplits[0]);
+        this.secondNum = Integer.parseInt(countSplits[1]);
         String character = wordScanner.next();
-        char target = character.charAt(0);
-        String password = wordScanner.next();
-        return  (password.charAt(firstIndex) == target) ^ (password.charAt(secondIndex) == target);
+        this.target = character.charAt(0);
+        this.password = wordScanner.next();
+    }
+
+    private static String[] getCountSplits(Scanner wordScanner) {
+        String count = wordScanner.next();
+        return count.split("-");
     }
 }
