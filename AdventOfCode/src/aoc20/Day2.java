97914fc80f2a654ba2c295c1e787be8371ae397d
Kellen Donohue <kdonohue@palantir.com>
2020-12-02 13:22:23 -0800

    day 2 part 2

--- AdventOfCode/src/aoc20/Day2.java
+++ AdventOfCode/src/aoc20/Day2.java
@@ -1,2 +1,41 @@
-package aoc20;public class Day2 {
+package aoc20;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count = 0;
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            if (line.isEmpty()) {
+                break;
+            }
+            boolean valid = processCase(line);
+            if (valid) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static boolean processCase(String line) {
+        Scanner wordScanner = new Scanner(line);
+        String count = wordScanner.next();
+        String[] countSplits = count.split("-");
+        int min = Integer.parseInt(countSplits[0]);
+        int max = Integer.parseInt(countSplits[1]);
+        String character = wordScanner.next();
+        char target = character.charAt(0);
+        String password = wordScanner.next();
+        int charCount = 0;
+        for (char c : password.toCharArray()) {
+            if (c == target) {
+                charCount++;
+            }
+        }
+        System.out.println(password);
+        return (charCount >= min) && (charCount <= max);
+    }
 }
