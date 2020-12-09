8abdbde694ff922cab779e78277e2b1e7025c0e8
Kellen Donohue <kdonohue@palantir.com>
2020-12-08 21:18:47 -0800

    day 8 2

--- AdventOfCode/src/aoc20/Day9.java
+++ AdventOfCode/src/aoc20/Day9.java
@@ -1,7 +1,9 @@
 package aoc20;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Set;
@@ -9,13 +11,37 @@ import java.util.Set;
 public class Day9 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        List<Long> allNumbers = new ArrayList<>();
         Queue<Long> queue = new LinkedList<>();
         Set<Long> set = new HashSet<>();
         for (int i = 0; i <= 25; i++) {
             long l = in.nextLong();
             queue.add(l);
             set.add(l);
+            allNumbers.add(l);
         }
+        long target = doPart1(in, queue, set, allNumbers);
+        for (int i = 0; i < allNumbers.size(); i++) {
+            long cumSum = allNumbers.get(i);
+            int j = i + 1;
+            while (cumSum < target) {
+                cumSum += allNumbers.get(j);
+                j++;
+            }
+            if (cumSum == target) {
+                long min = allNumbers.get(i);
+                long max = allNumbers.get(i);
+                for (int ii = i; ii < j; ii++) {
+                    min = Math.min(min, allNumbers.get(ii));
+                    max = Math.max(max, allNumbers.get(ii));
+                }
+                System.out.println(min + max);
+            }
+        }
+
+    }
+
+    private static long doPart1(Scanner in, Queue<Long> queue, Set<Long> set, List<Long> allNumbers) {
         while (true) {
             long next = in.nextLong();
             if (canAdd(set, next)) {
@@ -23,12 +49,12 @@ public class Day9 {
                 set.remove(removed);
                 queue.add(next);
                 set.add(next);
+                allNumbers.add(next);
             } else {
                 System.out.println(next);
-                break;
+                return next;
             }
         }
-
     }
 
     private static boolean canAdd(Set<Long> set, long next) {
