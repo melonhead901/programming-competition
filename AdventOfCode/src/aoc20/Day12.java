9428696e3d021c7b54c5b45f48a9678bea506d77
Kellen Donohue <kdonohue@palantir.com>
2020-12-11 21:55:03 -0800

    day 12 2

--- AdventOfCode/src/aoc20/Day12.java
+++ AdventOfCode/src/aoc20/Day12.java
@@ -25,8 +25,7 @@ public class Day12 {
 
         @Override
         public String toString() {
-            return "{" + degress +
-                    '}';
+            return "{" + degress + '}';
         }
 
         public Direction turnRight(int degress) {
@@ -36,7 +35,7 @@ public class Day12 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Status status = new Status(0, 0, new Direction(0));
+        Status status = new Status(0, 0, new WaypointPosition(10, 1, 0));
         while (in.hasNext()) {
             String line = in.nextLine();
             if (line.isBlank()) {
@@ -44,28 +43,85 @@ public class Day12 {
             }
             char c = line.charAt(0);
             int num = Integer.parseInt(line.substring(1));
-            status = status.processMove(c, num);
+            status = status.processMoveWaypoint(c, num);
             System.out.println(status);
             System.out.println(status.manhattanDistance());
         }
         System.out.println(status.manhattanDistance());
     }
 
+    static class WaypointPosition {
+        final int x, y;
+        int deg;
+
+        @Override
+        public String toString() {
+            return String.format("(%s,%s):%s", x, y, deg);
+        }
+
+        WaypointPosition(int x, int y, int deg) {
+            this.x = x;
+            this.y = y;
+            this.deg = deg;
+        }
+
+        public WaypointPosition moveY(int dy) {
+            return new WaypointPosition(x, y + dy, deg);
+        }
+
+        public WaypointPosition moveX(int dx) {
+            return new WaypointPosition(x + dx, y, deg);
+        }
+
+        public WaypointPosition rotate(int num) {
+            if (num < 0) {
+                num += 360;
+            }
+            switch (num % 360) {
+                case 90:
+                    return new WaypointPosition(-y, x, 0);
+                case 180:
+                    return new WaypointPosition(-x, -y, 0);
+                case 270:
+                    return new WaypointPosition(y, -x, 0);
+            }
+            throw new IllegalStateException((num % 360) + "");
+        }
+
+        public int dx(int num) {
+            return (int) (num * x);
+        }
+
+        public int dy(int num) {
+            return (int) (num * y);
+        }
+    }
+
     static class Status {
         final int x, y;
-        final Direction direction;
+        private final WaypointPosition waypointPosition;
+        Direction direction;
 
+        /*
         Status(int x, int y, Direction direction) {
             this.x = x;
             this.y = y;
             this.direction = direction;
         }
+        *        */
+
+        Status(int x, int y, WaypointPosition waypointPosition) {
+            this.x = x;
+            this.y = y;
+            this.waypointPosition = waypointPosition;
+        }
 
         @Override
         public String toString() {
-            return "Status{" + "x=" + x + ", y=" + y + ", direction=" + direction + '}';
+            return "Status{" + "x=" + x + ", y=" + y + ", waypoint=" + waypointPosition + '}';
         }
 
+        /*
         public Status processMove(char c, int num) {
             switch (c) {
                 case 'N':
@@ -82,6 +138,26 @@ public class Day12 {
                     return new Status(x, y, direction.turnRight(num));
                 case 'F':
                     return new Status(x + (direction.dx() * num), y + (direction.dy() * num), direction);
+            }
+            throw new IllegalStateException();
+        }
+         */
+        public Status processMoveWaypoint(char c, int num) {
+            switch (c) {
+                case 'N':
+                    return new Status(x, y, waypointPosition.moveY(num));
+                case 'E':
+                    return new Status(x, y, waypointPosition.moveX(num));
+                case 'S':
+                    return new Status(x, y, waypointPosition.moveY(-num));
+                case 'W':
+                    return new Status(x, y, waypointPosition.moveX(-num));
+                case 'L':
+                    return new Status(x, y, waypointPosition.rotate(num));
+                case 'R':
+                    return new Status(x, y, waypointPosition.rotate(-num));
+                case 'F':
+                    return new Status(x + waypointPosition.dx(num), y + waypointPosition.dy(num), waypointPosition);
             }
             throw new IllegalStateException();
         }
