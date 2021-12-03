cb67910ada88a90e98bd03e7a9ac5434967a034b
Kellen Donohue <kdonohue@palantir.com>
2021-12-02 21:52:27 -0800

    day 3 2

--- AdventOfCode/src/aoc21/Day3.java
+++ AdventOfCode/src/aoc21/Day3.java
@@ -1,20 +1,26 @@
 package aoc21;
 
+import com.google.common.collect.Iterables;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Day3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String first = "000000011010";
-        // String first = "00000";
+         String first = "000000011010";
+        //String first = "00000";
+        List<String> itemsSeen = new ArrayList<>();
         int[] onesCounts = new int[first.length()];
-        int numLines = 0;
         while (in.hasNext()) {
             String line = in.next();
             if (line.startsWith("Z")) {
                 break;
             }
+            itemsSeen.add(line);
             if (Objects.equals(line, "Z")) {
                 break;
             }
@@ -23,20 +29,32 @@ public class Day3 {
                     onesCounts[i]++;
                 }
             }
-            numLines++;
         }
-        int gamma = 0;
-        int epsilon = 0;
-        for (int i = 0; i < onesCounts.length; i++) {
-            gamma *= 2;
-            epsilon *= 2;
-            if (onesCounts[i] > (numLines / 2)) {
-                gamma++;
-            } else  {
-                epsilon++;
-            }
+        List<String> oxygenList = new ArrayList<>(itemsSeen);
+        for (int i = 0; (i < first.length()) && (oxygenList.size() > 1); i++) {
+            char matchBit = (countOnes(oxygenList, i) >= (oxygenList.size() / 2.0)) ? '1' : '0';
+            final int finalI = i;
+            oxygenList = oxygenList.stream().filter(li -> li.charAt(finalI) == matchBit).collect(Collectors.toList());
         }
-        System.out.printf("gamma: %s, epsilon: %s, result: %s\n", gamma, epsilon, gamma*epsilon);
+        List<String> carbonList = new ArrayList<>(itemsSeen);
+        for (int i = 0; (i < first.length()) && (carbonList.size() > 1); i++) {
+            char matchBit = (countOnes(carbonList, i) >= (carbonList.size() / 2.0)) ? '0' : '1';
+            final int finalI = i;
+            carbonList = carbonList.stream().filter(li -> li.charAt(finalI) == matchBit).collect(Collectors.toList());
+        }
+        System.out.println(oxygenList);
+        int oxy = Integer.parseInt(Iterables.getOnlyElement(oxygenList), 2);
+        System.out.println(oxy);
+        int carb = Integer.parseInt(Iterables.getOnlyElement(carbonList), 2);
+        System.out.println(carbonList);
+        System.out.println(carb);
+
+        System.out.println(oxy*carb);
+
+    }
+
+    private static int countOnes(List<String> oxygenMatch, int i) {
+        return (int) oxygenMatch.stream().filter(s -> s.charAt(i) == '1').count();
     }
 
 }
