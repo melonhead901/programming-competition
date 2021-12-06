fc33671eaf04fcec7ff8523bccef686a9c60f5fc
Kellen Donohue <kdonohue@palantir.com>
2021-12-05 21:46:28 -0800

    day 6 paryt 2

--- AdventOfCode/src/aoc21/Day6.java
+++ AdventOfCode/src/aoc21/Day6.java
@@ -1,7 +1,9 @@
 package aoc21;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Day6 {
@@ -13,21 +15,35 @@ public class Day6 {
         while (numScanner.hasNext()) {
             values.add(numScanner.nextInt());
         }
+        Map<Integer, Long> freqs = convertToMap(values);
         for (int i = 1; i <= 256; i++) {
-            values = createNewValues(values);
+            freqs = createNewValues(freqs);
             // System.out.printf("%s: %s %s \n", i, values.size(), values);
-            System.out.printf("%s: %s\n", i, values.size());
+
+            System.out.printf("%s: %s \n", i, freqs.values().stream().reduce(0L, Long::sum));
         }
     }
 
-    private static List<Integer> createNewValues(List<Integer> values) {
-        List<Integer> result = new ArrayList<>();
-        for (int i : values) {
-            if (i == 0) {
-                result.add(8);
-                result.add(6);
+    private static Map<Integer, Long> createNewValues(Map<Integer, Long> values) {
+        Map<Integer, Long> newFreqs = new HashMap<>();
+        for (int key : values.keySet()) {
+            if (key == 0) {
+                newFreqs.put(8, values.get(0));
             } else {
-                result.add(i-1);
+                newFreqs.put(key - 1, values.get(key));
+            }
+        }
+        newFreqs.put(6, newFreqs.getOrDefault(6, 0L) + newFreqs.getOrDefault(8, 0L));
+        return newFreqs;
+    }
+
+    private static Map<Integer, Long> convertToMap(List<Integer> values) {
+        Map<Integer, Long> result = new HashMap<>();
+        for (int val : values) {
+            if (result.containsKey(val)) {
+                result.put(val, result.get(val) + 1);
+            } else {
+                result.put(val, 1L);
             }
         }
         return result;
