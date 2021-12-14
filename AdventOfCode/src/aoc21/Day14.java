199b0b1599795f2918d8dcd8958bac3c18cd7d2f
Kellen Donohue <kdonohue@palantir.com>
2021-12-14 11:23:19 -0800

    day 14 part 2

--- AdventOfCode/src/aoc21/Day14.java
+++ AdventOfCode/src/aoc21/Day14.java
@@ -1,11 +1,12 @@
 package aoc21;
 
 
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 public class Day14 {
     public static void main(String[] args) {
@@ -17,17 +18,106 @@ public class Day14 {
             String line = in.nextLine();
             rules.put(line.substring(0, 2), line.charAt(line.length() - 1));
         }
-        int iterations = 40;
-        for (int i = 1; i <= iterations; i++) {
-            pattern = applyRules(pattern, rules);
-            System.out.printf("%s: %s\n", i, pattern.length());
+        CharCounter counter = new CharCounter();
+        Map<String, Long> patternCounts = new HashMap<>();
+        for (int i = 0; (i + 1) < pattern.length(); i++) {
+            String key = pattern.substring(i, i + 2);
+            increment(patternCounts, key, 1);
         }
-        Map<Character, Long> frequencies = createFreqMap(pattern);
-        List<Long> freqs = frequencies.values().stream().sorted().collect(Collectors.toList());
-        System.out.println(freqs);
-        System.out.println(freqs.get(freqs.size()-1) - freqs.get(0));
+        int iterations = 40;
+        for (int i = 0; i < iterations; i++) {
+            Map<String, Long> newCounts = new HashMap<>();
+            for (Map.Entry<String, Long> entry : patternCounts.entrySet()) {
+                char newChar = rules.get(entry.getKey());
+                String useFirst = String.format("%s%s", entry.getKey().charAt(0), newChar);
+                increment(newCounts, useFirst, entry.getValue());
+                String useSecond = String.format("%s%s", newChar, entry.getKey().charAt(1));
+                increment(newCounts, useSecond, entry.getValue());
+            }
+            patternCounts = newCounts;
+            System.out.println(patternCounts);
+        }
+        patternCounts.forEach((k, v) -> counter.increment(k.charAt(0), v));
+        patternCounts.forEach((k, v) -> counter.increment(k.charAt(1), v));
+        counter.divideBy2();
+        counter.increment(pattern.charAt(0));
+        counter.increment(pattern.charAt(pattern.length() - 1));
+        System.out.println(counter);
+        System.out.println(counter.difference());
+
+    }
+
+    static <K> void increment(Map<K, Long> counts, K key, long incrementAmount) {
+        counts.put(key, counts.getOrDefault(key, 0L) + incrementAmount);
+    }
+
+    private static void queueApproach(String pattern, Map<String, Character> rules, CharCounter counter) {
+        LinkedList<Character> linkedList = new LinkedList<>();
+        for (char c : pattern.toCharArray()) {
+            linkedList.addLast(c);
+        }
+        int iterDepth = 0;
+        int iterations = 2;
+        while (linkedList.size() >= 2) {
+            char first = linkedList.removeFirst();
+            counter.increment(first);
+            char second = linkedList.peekFirst();
+            Character next = rules.get(String.format("%s%s", first, second));
+            linkedList.addFirst(next);
+            iterDepth++;
+            if (iterDepth > iterations) {
+                linkedList.removeFirst();
+                iterDepth = 0;
+            }
+        }
+        counter.increment(linkedList.removeFirst());
+    }
+
+    private static char exceuteDescension(int iterations, char last, char curr, CharCounter counter, Map<String, Character> rules) {
+        counter.increment(last);
+        if (iterations <= 0) {
+            return curr;
+        }
+        char next = rules.get(String.format("%s%s", last, curr));
+        return exceuteDescension(iterations - 1, next, curr, counter, rules);
+    }
+
+    static class CharCounter {
+        public Map<Character, Long> counter;
+
+        public CharCounter() {
+            this(new HashMap<>());
+        }
+
+        public CharCounter(HashMap<Character, Long> newF) {
+            this.counter = newF;
+        }
 
 
+        void increment(char c, long l) {
+            System.out.print(c);
+            this.counter.put(c, counter.getOrDefault(c, 0L) + l);
+        }
+
+        void increment(char c) {
+            increment(c, 1L);
+        }
+
+        @Override
+        public String toString() {
+            return counter.toString();
+        }
+
+        public long difference() {
+            long min = counter.values().stream().min(Comparator.naturalOrder()).get();
+            long max = counter.values().stream().max(Comparator.naturalOrder()).get();
+            return max - min;
+        }
+
+        public void divideBy2() {
+            Set<Character> chars = this.counter.keySet();
+            chars.forEach(c -> counter.put(c, counter.get(c) / 2));
+        }
     }
 
     private static Map<Character, Long> createFreqMap(String pattern) {
