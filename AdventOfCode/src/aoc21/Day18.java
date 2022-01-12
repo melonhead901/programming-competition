1f8814accd75cf5390f60da34b5409a53841a5d7
Kellen Donohue <kdonohue@palantir.com>
2022-01-12 13:31:28 -0500

    day 18

---
+++ AdventOfCode/src/aoc21/Day18.java
@@ -0,0 +1,37 @@
+package aoc21;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Day18 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List<Pair> pairs = new ArrayList<>();
+        while (in.hasNextLine()) {
+            pairs.add(parsePair(in.nextLine()));
+
+        }
+
+    }
+
+    private static Pair parsePair(String nextLine) {
+        int commaIndex = nextLine.indexOf(',');
+        if (nextLine.charAt(0) == '[') {
+
+        }
+    }
+
+    static class Pair {
+        int leftVal;
+        Pair leftPair;
+        int rightVal;
+        Pair rightPair;
+
+        @Override
+        public String toString() {
+            return String.format("[%s, %s]", (leftPair != null) ? leftPair.toString() : leftVal, (rightPair != null) ? rightPair.toString() : rightVal);
+        }
+    }
+
+}
