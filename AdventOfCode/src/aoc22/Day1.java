225b1998729e4a3f5166edfedfb17ec5fc7696fa
Kellen Donohue <kdonohue@palantir.com>
2022-12-02 17:16:04 -0800

    day 1 part 1

--- AdventOfCode/src/aoc22/Day1.java
+++ AdventOfCode/src/aoc22/Day1.java
@@ -1,2 +1,22 @@
-package aoc22;public class Day1 {
+package aoc22;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int elfMax = 0;
+        while (in.hasNextLine()) {
+            int elfTotal = 0;
+            String next = "0";
+            while (!next.equals("")) {
+                int val = Integer.parseInt(next);
+                elfTotal += val;
+                next = in.nextLine();
+            }
+            elfMax = Math.max(elfMax, elfTotal);
+        }
+        System.out.println(elfMax);
+    }
 }
