ba174fff7b9d96de828f93b3c87ceb584c5dc491
Kellen Donohue <kdonohue@palantir.com>
2022-12-05 23:08:57 -0600

    day 17 2

--- AdventOfCode/src/aoc22/Day6.java
+++ AdventOfCode/src/aoc22/Day6.java
@@ -4,18 +4,21 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class Day6 {
+
+    public static final int LEN = 14;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         while (in.hasNextLine()) {
             String line = in.nextLine();
-            for (int i = 0; i < line.length() - 4; i++) {
-                String sub = line.substring(i, i + 4);
+            for (int i = 0; i < line.length() - LEN; i++) {
+                String sub = line.substring(i, i + LEN);
                 HashSet<Character> set = new HashSet<>();
-                for (int ii = 0; ii < 4; ii++) {
+                for (int ii = 0; ii < LEN; ii++) {
                     set.add(sub.charAt(ii));
                 }
-                if (set.size() == 4) {
-                    System.out.println(i + 4);
+                if (set.size() == LEN) {
+                    System.out.println(i + LEN);
                     break;
                 }
             }
