004d3525f7214466442bbd520c1b05871a45209d
Kellen Donohue <kdonohue@palantir.com>
2022-12-09 16:53:56 -0800

    day 9 2

--- AdventOfCode/src/aoc22/Day9.java
+++ AdventOfCode/src/aoc22/Day9.java
@@ -8,7 +8,7 @@ public class Day9 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         HashSet<Coord> tailPositions = new HashSet<>();
-        tailPositions.add(new Coord(0,0));
+        tailPositions.add(new Coord(0, 0));
         Coord[] knots = new Coord[10];
         for (int i = 0; i < knots.length; i++) {
             knots[i] = new Coord(0, 0);
@@ -30,19 +30,19 @@ public class Day9 {
                 newKnots[0] = newHead;
 
                 for (int t = 1; t < 10; t++) {
-                    newKnots[t] = updateTailPosition(newKnots[t-1], knots[t-1], knots[t]);
+                    newKnots[t] = updateTailPosition(newKnots[t - 1], knots[t - 1], knots[t]);
                 }
                 knots = newKnots;
-                printBoard(knots);
 
                 tailPositions.add(knots[9]);
             }
+            // printBoard(knots);
         }
         System.out.println(tailPositions.size());
     }
 
     private static void printBoard(Coord[] knots) {
-        int SIZE = 10;
+        int SIZE = 26;
         int[][] board = new int[SIZE][];
         for (int i = 0; i < SIZE; i++) {
             int[] row = new int[SIZE];
@@ -52,8 +52,8 @@ public class Day9 {
             board[i] = row;
         }
         for (int c = 0; c < knots.length; c++) {
-            int row = SIZE/2-knots[c].y-1;
-            int col = knots[c].x + SIZE/2;
+            int row = SIZE / 2 - knots[c].y - 1;
+            int col = knots[c].x + SIZE / 2;
             if (board[row][col] == -1) {
                 board[row][col] = c;
             }
@@ -62,7 +62,7 @@ public class Day9 {
             for (int j = 0; j < SIZE; j++) {
                 int val = board[i][j];
                 if (val == -1) {
-                    if (i == SIZE/2 && j == SIZE/2) {
+                    if (i + 1 == SIZE / 2 && j == SIZE / 2) {
                         System.out.print("s");
                     } else {
                         System.out.print(".");
@@ -74,7 +74,6 @@ public class Day9 {
             System.out.println();
         }
         System.out.println("--------------------------------");
-
     }
 
     private static void oldPrintBoard(Coord[] knots) {
@@ -87,22 +86,40 @@ public class Day9 {
     private static Coord updateTailPosition(@Nonnull Coord newHead, Coord head, Coord tail) {
         int xDiff = Math.abs(newHead.x - tail.x);
         int yDiff = Math.abs(newHead.y - tail.y);
-        if (xDiff + yDiff == 3) {
-            int newX, newY;
-            if (xDiff == 2) {
-                newX = (int) (0.5*(newHead.x+tail.x));
-                newY = newHead.y;
-            } else {
-                newX = newHead.x;
-                newY = (int) (0.5*(newHead.y+tail.y));
+        if (xDiff == 0) {
+            switch (yDiff) {
+                case 0:
+                case 1:
+                    return tail;
+                case 2:
+                    return new Coord(newHead.x, (newHead.y + tail.y) / 2);
             }
-            return new Coord(newX, newY);
+        } else if (xDiff == 1) {
+            switch (yDiff) {
+                case 0:
+                case 1:
+                    return tail;
+                case 2:
+                    int newX = newHead.x;
+                    int newY = (int) (0.5 * (newHead.y + tail.y));
+                    return new Coord(newX, newY);
+            }
+        } else if (xDiff == 2) {
+            switch (yDiff) {
+                case 0:
+                    return new Coord((newHead.x + tail.x) / 2, newHead.y);
+                case 1:
+                    int newX = (int) (0.5 * (newHead.x + tail.x));
+                    int newY = newHead.y;
+                    return new Coord(newX, newY);
+                case 2:
+                    newX = (int) (0.5 * (newHead.x + tail.x));
+                    newY = (int) (0.5 * (newHead.y + tail.y));
+                    return new Coord(newX, newY);
+            }
         }
-        if (xDiff == 2 || yDiff == 2) {
-            return head;
-        }
-        return tail;
 
+        throw new IllegalStateException("Unexpected case, xDiff:" + xDiff + " yDiff: " + yDiff);
     }
 
     static class Coord {
@@ -121,19 +138,18 @@ public class Day9 {
         public Coord left() {
             return new Coord(this.x - 1, y);
         }
+
         public Coord up() {
-            return new Coord(this.x , y+ 1);
+            return new Coord(this.x, y + 1);
         }
+
         public Coord down() {
-            return new Coord(this.x , y-1);
+            return new Coord(this.x, y - 1);
         }
 
         @Override
         public String toString() {
-            return "{" +
-                     + x +
-                    "," + y +
-                    '}';
+            return "{" + +x + "," + y + '}';
         }
 
         @Override
